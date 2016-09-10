042a21c7ec21fb35a4cee2372ee5793bf672c495
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2016-09-10 12:33:24 +0700

    Updates

---
+++ app/src/main/java/com/example/lp700/mvcstructure/MainApplication.java
@@ -0,0 +1,19 @@
+package com.example.lp700.mvcstructure;
+
+import android.app.Application;
+
+/**
+ * Created by lp700 on 10/9/2559.
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
