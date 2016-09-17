5be67f55715a8faabd02d654cb225340c5720362
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2016-09-17 11:04:51 +0700

    Updates

--- app/src/main/java/com/example/lp700/mvcstructure/activity/MainActivity.java
+++ app/src/main/java/com/example/lp700/mvcstructure/activity/MainActivity.java
@@ -27,10 +27,10 @@ public class MainActivity extends AppCompatActivity {
         int screenWidth = ScreenUtils.getInstance().getScreenWidth();
         int screenHeight = ScreenUtils.getInstance().getScreenHeight();
 
-        Toast.makeText(getApplicationContext(),"Width : " + screenWidth + " Height : " + screenHeight, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Width : " + screenWidth + " Height : " + screenHeight, Toast.LENGTH_SHORT).show();
 
         // Separate by Android Version
-        if (Build.VERSION.SDK_INT >= 21){
+        if (Build.VERSION.SDK_INT >= 21) {
             // Run on Android 21+
 
         } else {
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // แปะ Fragment ที่ Activity  -- Fragment แปะครั้งแรกครั้งเดียวไม่ต้องแปะซ้ำ ระบบจะจัดการให้ตอนที่ Activity เกิดใหม่
-        if (savedInstanceState == null){
+        if (savedInstanceState == null) { // แปะตอนที่ Activity ถูกสร้างขึ้นมาครั้งแรก
             // First Created
             // Place Fragment here
             getSupportFragmentManager().beginTransaction()
@@ -47,16 +47,24 @@ public class MainActivity extends AppCompatActivity {
                             MainFragment.newInstance(123),
                             "MainFragment")
                     .commit();
+
+            SecondFragment secondFragment = SecondFragment.newInstance();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentContainer,
+                            secondFragment,
+                            "SecondFragment")
+                    .detach(secondFragment)
+                    .commit();
         }
     }
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        if (savedInstanceState == null){
+        if (savedInstanceState == null) {
             MainFragment fragment = (MainFragment)
                     getSupportFragmentManager().findFragmentByTag("MainFragment");
-            fragment.setHelloText("Woo Hooooooo");
+            fragment.setHelloText("Woo Hooooooo\nWoo Hooooooo\\Woo Hooooooo\\Woo Hooooooo\\Woo Hooooooo\\Woo Hooooooo\\Woo Hooooooo\\Woo Hooooooo\\Woo Hooooooo\\Woo Hooooooo\\Woo Hooooooo\\Woo Hooooooo\\Woo Hooooooo\\Woo Hooooooo\\Woo Hooooooo\\Woo Hooooooo\\Woo Hooooooo\\Woo Hooooooo\\");
         }
     }
 
@@ -69,12 +77,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_second_fragment:
+            case R.id.action_second_fragment: {
 
-                Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.contentContainer);
+                Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.contentContainer); // ค้นหา fragment ตัวที่อยู่บนสุด
 
                 if (fragment instanceof SecondFragment == false) {
                     getSupportFragmentManager().beginTransaction()
+//                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                             .setCustomAnimations(R.anim.from_right, R.anim.to_left, R.anim.from_left, R.anim.to_right)
                             .replace(R.id.contentContainer, SecondFragment.newInstance())
                             .addToBackStack(null)
@@ -83,6 +92,32 @@ public class MainActivity extends AppCompatActivity {
 
                 Toast.makeText(MainActivity.this, "Second Fragment", Toast.LENGTH_SHORT).show();
                 return true;
+            }
+            case R.id.action_first_tab: {
+                MainFragment mainFragment = (MainFragment)
+                        getSupportFragmentManager().findFragmentByTag("MainFragment");
+                SecondFragment secondFragment = (SecondFragment)
+                        getSupportFragmentManager().findFragmentByTag("SecondFragment");
+                getSupportFragmentManager().beginTransaction()
+                        .attach(mainFragment)
+                        .detach(secondFragment)
+                        .addToBackStack(null)
+                        .commit();
+                return true;
+            }
+            case R.id.action_second_tab: {
+                MainFragment mainFragment = (MainFragment)
+                        getSupportFragmentManager().findFragmentByTag("MainFragment");
+                SecondFragment secondFragment = (SecondFragment)
+                        getSupportFragmentManager().findFragmentByTag("SecondFragment");
+                getSupportFragmentManager().beginTransaction()
+                        .attach(secondFragment)
+                        .detach(mainFragment)
+                        .addToBackStack(null)
+                        .commit();
+                return true;
+            }
+
         }
         return super.onOptionsItemSelected(item);
     }
