85007c315bcf2d55e8cf193431555bc01ca9e712
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2016-09-17 11:43:19 +0700

    Updates

--- app/src/main/java/com/example/lp700/mvcstructure/MainApplication.java
+++ app/src/main/java/com/example/lp700/mvcstructure/MainApplication.java
@@ -3,6 +3,8 @@ package com.example.lp700.mvcstructure;
 import android.app.Application;
 
 import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by lp700 on 10/9/2559.
@@ -12,6 +14,7 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         // Initialize thing(s) here
         Contextor.getInstance().init(getApplicationContext()); // การเอา Application Context มาฝากที่ Contextor
